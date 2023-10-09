db19d848967a60e166d604bf54086a200452ad59
LhamCode <lmoura.ha@gmail.com>
2023-10-09 17:16:46 -0300

    iniciando js

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botoes = document.querySelectorAll('.botao');
+
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+
+            const botaoSelecionado = document.querySelector(".botao.selecionado")
+            botaoSelecionado.classList.remove("selecionado")
+
+            botao.classList.add("selecionado")
+        });
+});
+
+//botoes.addEventListener('click', () => {
+//    console.log("clicou no botão")
+//});
